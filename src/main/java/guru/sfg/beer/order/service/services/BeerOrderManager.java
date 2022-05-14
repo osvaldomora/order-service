aa14f0c52f00050a9bdd6e16add2fd09efe99aad
Jose Esquinca <esquinca_jose@hotmail.com>
2022-05-14 13:34:30 -0500

    solving issues

---
+++ src/main/java/guru/sfg/beer/order/service/services/BeerOrderManager.java
@@ -0,0 +1,24 @@
+package guru.sfg.beer.order.service.services;
+
+import guru.sfg.beer.order.service.domain.BeerOrder;
+import guru.sfg.brewery.model.BeerOrderDto;
+
+import java.util.UUID;
+
+
+public interface BeerOrderManager {
+
+    BeerOrder newBeerOrder(BeerOrder beerOrder);
+
+    void processValidationResult(UUID beerOrderId, Boolean isValid);
+
+    void beerOrderAllocationPassed(BeerOrderDto beerOrder);
+
+    void beerOrderAllocationPendingInventory(BeerOrderDto beerOrder);
+
+    void beerOrderAllocationFailed(BeerOrderDto beerOrder);
+
+    void beerOrderPickedUp(UUID id);
+
+    void cancelOrder(UUID id);
+}
